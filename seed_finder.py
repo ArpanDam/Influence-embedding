a4e6e37eead8fdaf27428d66a87bd0fc022ae3d2
ArpanDam <arpandam1@gmail.com>
2024-04-24 10:16:23 +0530

    Add files via upload

--- seed_finder.py
+++ seed_finder.py
@@ -80,11 +80,11 @@ def func(k):
                 
             # sort dict1 and take the 1st key into seed    
         sorted_dict = dict(sorted(dict1.items(), key=lambda item: item[1], reverse=True))
-        if (len(seed)==4):
-            print("")
+        #if (len(seed)==4):
+            #print("")
         first_key = next(iter(sorted_dict))
         seed.append(first_key)
-        print(seed)
+        #print(seed)
        
         for follower in dict2[first_key]:
             influenced_member_2.add(follower) # infliuenced member 2 should contain all the member influenced for k-1 seed
@@ -94,88 +94,9 @@ def func(k):
             influenced_member.add(m)
     
     #return seed, len(influenced_member),influenced_member
-    return seed,influenced_member
+    #return seed,influenced_member
+    return seed
     
-    
-result=func(5)
-print(result)
-print("")
+#result=func(5)
 #print(result)
-#print("")    
-#func(5)
-'''
-length=11
-influenced=[]
-i=0
-for i in range(length):
-    
-    influenced.append(func(i))
-print(influenced)
 
-plt.plot(range(len(influenced)), influenced, marker='o', color='green', linestyle='-')
-plt.xlabel('Index')
-plt.ylabel('Values')
-plt.title('Line Graph for list1')
-plt.grid(True)
-plt.show()  
-'''  
-'''
-def func(k):
-    seed=[]
-    dict1={} # key influencer and value number of members influenced
-    dict2={} # key influencer value list of influenced members
-    for key in influenced_multiple_hop:
-        list1=[] # for each influencer
-        
-        mc=10
-        simulation=0
-        while simulation < mc:
-            
-            sum1=0   # for each simulation
-            for i in probability_of_influence[key]:
-                if(random.uniform(0, 1)<i[1]):
-                    sum1=sum1+1 # number of member influenced
-                    if simulation ==mc-1:
-                        if key in dict2:
-                            list2=dict2[key]
-                            list2.append(i[0])
-                            list2=set(list2)
-                            list2=list(list2)
-                            dict2[key]=list2
-                        else:
-                            list2=[]
-                            list2.append(i[0])
-                            dict2[key]=list2
-            list1.append(sum1)
-            simulation=simulation+1
-        dict1[key]=sum(list1)/len(list1)      
-        
-        
-    # sort dict1 and take the 1st key into seed    
-    sorted_dict = dict(sorted(dict1.items(), key=lambda item: item[1], reverse=True))
-    
-    first_key = next(iter(sorted_dict))
-    seed.append(first_key)
-    
-    
-    
-    if (len(seed)>k):
-        return seed
-'''  
-#seed=func(5)    
-New_algorirhm=[0, 49, 72, 99, 132, 136, 162, 175, 195,202]
-RRS=[0.0, 60.53, 109.157, 138.436, 157.395, 172.559, 191.369, 199.238, 203, 207.897]
-plt.plot(range(len(New_algorirhm)), New_algorirhm, marker='o', label='RRS', linestyle='-')
-
-# Plotting a line graph for list2
-plt.plot(range(len(RRS)), RRS, marker='s', label='DL', linestyle='--')
-
-plt.xlabel('Seed')
-plt.ylabel('Numberof influenced')
-plt.title('Line Graph Comparison')
-plt.legend()  # Show legend with labels
-plt.grid(True)
-plt.savefig('RRS vs DL.png')
-plt.show()
-
-print("")
